ce2e25e4d56a61c175982ac9d10086490034877b
Sahi <sahi.gonsangbeu@etu.unice.fr>
2021-06-26 14:42:07 +0200

    first commit

---
+++ genbank2fasta.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import argparse
+
+def lit_fichier(filename):
+    """" return a list containing the lines of the file """
+    with open(filename,'r') as f:
+        return f.readlines()
+        
+
+
+def extrait_organisme(lines_list):
+    organisme = ""
+    for line in lines_list:
+        if line[:10] == " ORGANISM":
+            # on récupère l'organisme du caractère 12 jusqu'à l'avant dernier
+            organisme = line[12:-1]
+    return organisme
+    
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_file', default=None)
+    args = parser.parse_args()
+    s_file = args.source_file
+    liste_des_lignes = lit_fichier(s_file)
+    print("Nombre de lignes lues {}:{}".format(liste_des_lignes,len(liste_des_lignes)))
+    nom_organisme = extrait_organisme(liste_des_lignes)
+    print("oragnisme :", nom_organisme)
+
+
+if __name__=='__main__':
+    main()
+
---
+++ words_in_proteome.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+import argparse
+
+
+def read_words(filename):
+    """
+    read the words contained in the file and return a list of the words converted to upercase and containing 3 characters or more
+    """
+
+    with open(filename) as f:
+        lines = f.readlines()
+        new_lines = []
+        for line in lines:
+            if len(line) >= 3:
+                line = line.replace('\n', '')
+                new_lines.append(line.upper())
+        return new_lines
+
+
+def read_sequences(filename):
+    """
+    read the proteome in the file and return a dictionary containing the protein sequences
+    """
+    with open(filename,'r') as fasta:
+        seq_dico = {}
+        for line in fasta:
+            if line.startswith('>sp'):
+                line = line.lstrip('>sp')
+                line = line.lstrip(' ')
+                tab = line.split('|')
+                proteine_id = tab[1]
+                line = line.rstrip(' ')
+                seq_dico[proteine_id] = ''
+            else:
+                line = line.rstrip('\n')
+                seq_dico[proteine_id] += line
+        return seq_dico
+
+def search_word_in_proteome(seq_dico, word_list):
+    """
+    Search and counts words in proteome sequences.
+
+    """
+
+    found_word_dict = {}
+    for word in word_list:
+        seq_count = 0
+        for prot_id in seq_dico:
+            if word in seq_dico[prot_id]:
+                seq_count += 1
+        if seq_count != 0:
+            found_word_dict[word] = seq_count
+            print("{} found {} in sequences".format(word, seq_count))
+    return found_word_dict
+
+
+def find_most_frequent_word(freq_dict):
+    """
+    Find and print the most frequent word found in sequences.
+
+    """
+    maxi = max(freq_dict.values())
+    for word in freq_dict:
+        if freq_dict[word] == maxi:
+            print("=> {} found in {} sequences".format(word, maxi))
+            # print("=> {} found {} times".format(word, maxi))
+
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_file', default=None)
+    parser.add_argument('source_file2', default=None)
+    args = parser.parse_args()
+    s_file = args.source_file
+    s_file2 = args.source_file2
+    common_words = read_words(s_file)
+    sequences = read_sequences(s_file2)
+    found_word = search_word_in_proteome(sequences, common_words)
+    most_frequent_word = find_most_frequent_word(found_word)
+    #print(f'Il y a {len(common_words)} mots dans cette sélection.\n')
+    #print(f" la séquence de la protéine Q8N4C6 est {sequences['Q8N4C6']}")
+
+
+if __name__ == '__main__':
+    main()
+
