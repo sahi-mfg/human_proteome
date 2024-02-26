810dda6cc41a7f9b3ca7943ab5bf2a2df4517d85
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-02-26 17:56:57 +0100

    Update command line arguments and variable names

--- main.py
+++ main.py
@@ -14,19 +14,20 @@ from src.words_in_proteome import (
     read_words,
 )
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("source_file", default=None)
-    parser.add_argument("source_file2", default=None)
-    parser.add_argument("source_file3", default=None)
+    parser.add_argument("english_common_words", default=None)
+    parser.add_argument("proteome_fasta", default=None)
+    parser.add_argument("gbk_file", default=None)
     args = parser.parse_args()
-    s_file = args.source_file
-    s_file2 = args.source_file2
-    s_file3 = args.source_file3
-    common_words = read_words(s_file)
-    sequences = read_sequences(s_file2)
-    list_of_lines = read_file(s_file)
-    print("Number of lines read {}:{}".format(list_of_lines, len(list_of_lines)))
+    english_common_words = args.english_common_words
+    proteome_fasta = args.proteome_fasta
+    gbk_file = args.gbk_file
+    common_words = read_words(english_common_words)
+    sequences = read_sequences(proteome_fasta)
+    list_of_lines = read_file(english_common_words)
+    print(f"Number of lines read {list_of_lines}:{list_of_lines}")
     name_organism = extract_organism(list_of_lines)
     print("organism :", name_organism)
     found_word = search_word_in_proteome(sequences, common_words)
